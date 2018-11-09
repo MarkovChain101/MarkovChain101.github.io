00f0a011305800855024dc6260cf55e074e54fb1
Ilya <ilya.yurchenko@tufts.edu>
2018-11-08 21:43:59 -0500

    index commit

--- index.js
+++ index.js
@@ -9,5 +9,11 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index')) 
-  .get('/cool', (req,res) => res.send(cool()))
+  .get('/scores.json', (req,res) => res.send('[]'))
+  .post('/submit', (req,res) => {
+	res.header("Acess-Control-Allow-Origin", "*");
+	res.header("Acess-Control-Allow-Headers", "Origin, X-Requested-With,  Content, Accept");
+	next();
+	res.send('[]');
+})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
